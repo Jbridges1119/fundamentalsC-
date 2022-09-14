c129a1077c609a719c85345464732f6b69b44b70
Jeff <jbridges1119@gmail.com>
2022-09-14 13:18:39 -0600

    Iteration

---
+++ OperatorsExpressionsStatements/Program.cs
@@ -0,0 +1,13 @@
+﻿//Variable declaration
+int x, y, a, b;
+
+//Assignment operator
+x = 3;
+y = 2;
+a = 1; 
+b = 1;
+
+//There are many mathematical operators...
+
+// && - and  || - or exist
+// truthy ? true : false 
