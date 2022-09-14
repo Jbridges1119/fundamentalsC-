1652fa9aa45d974065272b712fde753b786de181
Jeff <jbridges1119@gmail.com>
2022-09-14 15:07:16 -0600

    Test Methods

---
+++ SimpleMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//Not sure how to do this with .net 6 - come back once I know more
+console.WriteLine("Results: " + result);
+
+ReverseString("Hello");
+
+static void ReverseString(string message)
+{
+  char[] messageArray = message.ToCharArray();
+  Array.Reverse(messageArray);
+  foreach (char item in messageArray)
+  {
+   Result += item;   
+  }
+}
