976a911e8e40df647741045041520f1569921e8f
Jeff <jbridges1119@gmail.com>
2022-09-14 13:47:17 -0600

    Test arrays and how to iterate through

---
+++ Array/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = new int[5];
+//declare an array of numbers named numbers
+//create the array with new and declare number and length int[5]
+
+
+int[] numbers2 = new int[] {4,5,6,7,44,54};
+string[] names = new string[] { "eddie", "alex", "Mike"};
+
+foreach (string name in names)
+{
+  Console.WriteLine(name);
+}
+
+string zig = "You can get what you want out of life if you help other people get what they want.";
+
+//declare an array of characters
+char[] charArray = zig.ToCharArray();
+//revers array
+Array.Reverse(charArray);
+Console.Write(charArray);
+//iterate through reversed array
+foreach (char item in charArray)
+{
+  Console.Write(item);
+}
