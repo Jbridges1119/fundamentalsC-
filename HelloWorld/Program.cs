a4c859889066a1bc49f213e09ed4be263600ba4d
Jeff <jbridges1119@gmail.com>
2022-09-14 11:53:19 -0600

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// int x;
+// int y;
+
+// x = 7;
+// y = x + 3;
+
+// Console.WriteLine(y);
+// Console.ReadLine();
+
+Console.WriteLine("What is your name?");
+Console.Write("Type your first name: ");
+string myFristName;
+myFristName = Console.ReadLine();
+
+string myLastName; 
+Console.Write("Type your last name: ");
+myLastName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + myFristName + " " + myLastName);
+Console.ReadLine();
